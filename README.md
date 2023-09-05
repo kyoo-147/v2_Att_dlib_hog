d2bedd21418b41d8f6a3b93b7d305be2e696a6c1
kyoo1332 <ngoctuanvinh1332@gmail.com>
2023-09-05 12:35:19 +0700

    hello world

---
+++ README.md
@@ -0,0 +1,46 @@
+-------------Vesion 2 Update Face Atthendance Recognition - AI-Lab
+##########################
+
+Mở đầu:
+** Phát hiện và nhận diện cá nhân đơn thể hoặc nhiều khuôn mặt cùng lúc từ camera
+ # Giao diện đăng kí khuôn mặt với Tkinter
+ # Khi máy ảnh quá gần với khuôn mặt, out of ROI khỏi vùng camera-cảnh báo Out Of Range sẽ xuất hiện
+ # Tạo cơ sở dữ liệu từ hình ảnh chụp từ camera
+ # Nhận diện khuôn mặt-phát hiện và nhận diện với mỗi khung frame hình-đối với single face-sẽ chỉ thực thi nhận dạng một khuôn mặt mới xuất hiện để cải thiện FPS
+ # Sử dụng OT để đạt chất lượng nhận dạng cải thiện FPS cho mọi frame 
+ # Hỗ trợ show tên bằng tiếng trung quốc
+
+** Về độ chính xác
+ # Khi sử dụng ngưỡng khoảng cách "0.6" mô hình dlib có thể đạt độ chính xác lên đến ``99.38%`` trên tiêu chuẩn nhận diện khuôn mặt LFW tiêu chuẩn
+
+** Về thuật toán
+ # Mô hình CNN dựa trên Residual Neural Network(Mạnh nơ-ron hồi quy)
+ # Đây là mô hình mạng ResNet với 29 lớp tối ưu
+ # về cơ bản, đây là một phiên bản của ResNet-34 Network từ bài báo Deep Residual Learning cho Image Recognition bởi nhóm He, Zhang, Ren và Sun với một số lớp đã được loại bỏ và số lượng bộ lọc mỗi khung hình được giảm đi một nửa.
+
+** Tổng quan
+ #  Quá trình triển khai nhận dạng khuôn mặt trong dự án, phát hiện và nhận dạng cho mọi frame
+    + Không OT, nhận diện từng frame, nhận dạng
+    + Với OT, phát hiện khung hình ban đầu, khung tiếp theo, theo dõi trọng tâm-OT được sử dụng
+ # Sử đụng OT có tiết kiệm được thời gian tính toán bộ mô tả khuôn mặt để cải thiện FPS
+
+** Source Code
+** Install package
+ # pip install -r requirements.txt
+---------------How to run models-------------------------
+** Giao diện đăng kí tên người dùng: open cmd in folder save model: run: python get_faces_from_camera_tkinter.py
+or
+** Giao diện OpenCV - tương tự như bước trên: run: python get_face_from_camera.py
+** Train model, trích xuất tính năng và lưu trữ vào features_all.csv: run: python features_extraction_to_csv.py
+** Nhận diện khuôn mặt trên thời gian thực: python face_reco_from_camera.py
+** Nhận diện trên thời gian thực với FPS tốt hơn: python face_reco_from_camera_single_face.py
+** Nhận diện với OT-FPS tốt nhất: python face_reco_from_camera_ot.py
+
+** Sử dụng bộ dò khuôn mặt phía trước Dlib(HOG)
+** Khi chạy file train-từ tệp hình ảnh đã lưu ở bước trước trích xuất dữ liệu và các điểm tọa độ trên khuôn mặt và lưu vào file csv
+    + kích thước: ``n*129`` n là số hình ảnh*129 có nghĩa là tên khuôn mặt + 128D là tính ăng của khuôn mặt
+** Khi chạy chương trình nhận diện-chương trình sẽ so sánh dữ liệu khuôn mặt đã chụp với dữ liệu khuôn mặt được lưu trữ trước đó để tính khoảng cách Euclide
+-- sửa đổi cập nhật nhật kí ``logging.basicConfig(level=logging.DEBUG)`` để in thông tin cho mọi khung hình nếu cần, mặc định sẽ là ``logging.INFO``    
+
+"# v2_X_aTt" 
+"# v2_Att_dlib_hog" 
---
+++ X_Main.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+import tkinter as tk
+from tkinter import Message ,Text
+from PIL import Image, ImageTk
+import pandas as pd
+#import datetime
+from tkinter import font as tkFont
+from tkinter import PhotoImage 
+import time
+import tkinter.ttk as ttk
+import subprocess
+import logging
+# class Close():        
+#     def close_X_Main(self):
+#         self.X_Main.destroy()
+
+class X_Main():
+    def __init__(self):
+        # Create info ui
+        #self.master = master
+        self.xmain = tk.Tk()
+        self.xmain.title("X_Main - @minhcuong-AILab")
+        self.xmain.geometry("1300x800")
+        # Background main x
+        self.background_image = PhotoImage(file="uii/main0.png")
+        self.background_label = tk.Label(self.xmain, image=self.background_image)
+        self.background_label.place(relwidth=1, relheight=1)
+        # Main gui
+        #self.xmain.attributes('-fullscreen', True) setting fullscreen image
+        self.button_X_Register = tk.Button(self.xmain, text="ĐĂNG KÍ",command=self.open_X_Register,fg="white",anchor='w',justify='left',borderwidth=0  ,width=9  ,height=1 , activebackground = "Red" ,font=('Helvetica', 15, ' bold '), background="#07080A")#.grid(row=8, column=2, padx=5)
+        self.button_X_Register.place(x=310, y=193)
+        self.button_X_Register = tk.Button(self.xmain, text="HUẤN LUYỆN",command=self.open_X_Training,fg="white",anchor='w',justify='left',borderwidth=0  ,width=11  ,height=1 , activebackground = "Red" ,font=('Helvetica', 15, ' bold '), background="#07080A")#.grid(row=8, column=2, padx=5)
+        self.button_X_Register.place(x=310, y=310)
+        self.button_X_Recognition = tk.Button(self.xmain, text="NHẬN DIỆN",command=self.open_X_Recog,anchor='w',justify='left' ,fg="white",borderwidth=0    ,width=9  ,height=1, activebackground = "Red" ,font=('Helvetica', 15, ' bold '), background="#07080A")
+        self.button_X_Recognition.place(x=310, y=430)
+        self.button_X_Out = tk.Button(self.xmain, text="THOÁT",command=self.out_X_Main,anchor='w',justify='left' ,fg="white",borderwidth=0    ,width=9  ,height=1, activebackground = "Red" ,font=('Helvetica', 15, ' bold '), background="#07080A")
+        self.button_X_Out.place(x=310, y=548)
+    # Open X_Register Part---
+    def open_X_Register(self):
+        print("******      X_Register @minhcuong-AILab Already!")
+        self.X_Register_Path = "X_Register.py"
+        subprocess.call(['python', self.X_Register_Path], shell=True)
+    # Open X_Training Part---
+    def open_X_Training(self):
+        print("******      X_Training @minhcuong-AILab Already!")
+        self.X_Training_Path = "X_Training.py"
+        subprocess.call(['python', self.X_Training_Path], shell=True)
+    def open_X_Recog(self):
+        print("******      X_Recog_OT @minhcuong-AILab Already!")
+        self.X_Recog_OT_Path = "X_Recog.py"
+        subprocess.call(['python', self.X_Recog_OT_Path], shell=True)
+    # Out X_Main Program---
+    def out_X_Main(self):
+        self.xmain.destroy()
+        print("******      X_Main - @minhcuong-AILab ~.~ Hope you enjoy that!!!")
+    def run(self):
+        self.xmain.mainloop()
+
+
+def main():
+    # logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(filename="app.log", level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
+    logging.debug("Nhớ debug nha!!!")
+    logging.info("Check kĩ thông tin đi")
+    logging.warning("Đây là cảnh báo cho bạn nè!!!")
+    logging.error("Lỗi chương trình rồi bạn ơi!!!")
+    logging.critical("Bạn đã làm cái gì vậy?")
+    running = X_Main()
+    running.run()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
